e66458d33d290856faae48b179b81f4831dfc9a4
Bengula Jacob <kbengula@student.maseno.ac.ke>
2026-05-27 14:23:24 +0300

    repaired bluetooth

--- flakes/modules/bluetooth.nix
+++ flakes/modules/bluetooth.nix
@@ -1,6 +1,17 @@
 { config, pkgs, ... }:
 
 {
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        Enable = "Source,Sink,Media,Socket";
+      };
+    };
+  };
+
+  # Enable Blueman service for D-Bus integration and Polkit permission management (crucial for non-root users in 'bluetooth' group)
+  services.blueman.enable = true;
 }
